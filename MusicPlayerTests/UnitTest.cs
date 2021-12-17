20868c78a8e9a9e33c151e68081636635feb8813
Magnus Cook <69338558+SonicYeager@users.noreply.github.com>
2021-12-17 22:22:16 +0100

    ➕: added test project;

---
+++ MusicPlayerTests/UnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MusicPlayerTests
+{
+    public class UnitTestSample
+    {
+        [Fact]
+        public void Test_in_Out()
+        {
+
+        }
+    }
+}
