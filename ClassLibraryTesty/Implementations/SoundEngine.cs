a691e23eae7fcf3f94d8754023a22f409ba989d4
Magnus Cook <69338558+SonicYeager@users.noreply.github.com>
2021-12-22 18:56:16 +0100

    📝: Implementations/SoundEngine.cs constructor impl and added xml doc for SoundEngine class;

--- ClassLibraryTesty/Implementations/SoundEngine.cs
+++ ClassLibraryTesty/Implementations/SoundEngine.cs
@@ -6,13 +6,30 @@ using System.Collections.Generic;
 
 namespace MusicPlayerBackend
 {
+    /// <summary>
+    /// Implements <see cref="ISoundEngine"/> for handling of audiofiles and communication to audiodevices.
+    /// The underlying API can be subject to change, but the output and consistensy will remain.
+    /// Playing happens async.
+    /// </summary>
+    /// <inheritdoc cref="ISoundEngine"/>
     public class SoundEngine : ISoundEngine
     {
         public int ActualStream { get; set; }
-        public int ActualDevice { get; set; } = 4;
+        public int ActualDevice { get; set; }
+        private List<string> Devices { get; set; }
+
         public SoundEngine()
         {
-            Bass.Init(ActualDevice);
+            for (int i = 1; i < Bass.DeviceCount; ++i)
+            {
+                var deviceInfo = Bass.GetDeviceInfo(i);
+                if (deviceInfo.IsEnabled)
+                {
+                    Devices.Add(deviceInfo.Name);
+                    if(deviceInfo.IsDefault)
+                        ActualDevice = i;
+                }
+            }
         }
 
         ~SoundEngine()
