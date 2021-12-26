e80d536ef87c04d656aa4737b06fe13b5edcd6b0
Magnus Cook <69338558+SonicYeager@users.noreply.github.com>
2021-12-26 21:23:03 +0100

    📝: Implementations/JSONDeserializer.cs test first; * added correspondign doc; * GetHashCode tests for all internal types;

--- ClassLibraryTesty/Contracts.cs
+++ ClassLibraryTesty/Contracts.cs
@@ -183,9 +183,19 @@ namespace MusicPlayerBackend
             public string Serialize<T>(T serializable); 
         }
 
+        /// <summary>
+        /// Contracts the neccessary functions to allow deserializing of objects.
+        /// </summary>
         public interface IJSONDeserializer
         {
-            public T Serialize<T>(string deserializable);
+
+            /// <summary>
+            /// Describes the neccessary input and output for deserialization.
+            /// </summary>
+            /// <typeparam name="T"></typeparam>
+            /// <param name="deserializable"></param>
+            /// <returns> Should be an representation of the string as <typeparamref name="T"/>.</returns>
+            public T Deserialize<T>(string deserializable);
         }
 
         public interface IFileWriter
--- MusicPlayerTests/UnitTests/InternalTypes_UnitTests.cs
+++ MusicPlayerTests/UnitTests/InternalTypes_UnitTests.cs
@@ -35,6 +35,18 @@ namespace MusicPlayerTests
 
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void GetHashCode_FromFilled_SameAsAnEqual()
+        {
+            AudioDeviceModel i = new AudioDeviceModel() { Text = "SomeDevice" };
+            AudioDeviceModel ii = new AudioDeviceModel() { Text = "SomeDevice" };
+
+            int expected = i.GetHashCode();
+            int actual = ii.GetHashCode();
+
+            Assert.Equal(expected, actual);
+        }
     }
 
     public class ThemesModel_UnitTests
@@ -64,6 +76,18 @@ namespace MusicPlayerTests
             string expected = "Text: SomeTheme";
 
             string actual = testy.ToString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetHashCode_FromFilled_SameAsAnEqual()
+        {
+            ThemesModel i = new ThemesModel() { Text = "Sometheme" };
+            ThemesModel ii = new ThemesModel() { Text = "Sometheme" };
+
+            int expected = i.GetHashCode();
+            int actual = ii.GetHashCode();
 
             Assert.Equal(expected, actual);
         }
@@ -98,6 +122,18 @@ namespace MusicPlayerTests
             string actual = testy.ToString();
 
             Assert.Equal(ecpected, actual);
+        }
+
+        [Fact]
+        public void GetHashCode_FromFilled_SameAsAnEqual()
+        {
+            AudioDataModel i = new AudioDataModel() { Title = "RapGod", Duration = "00:00:00" };
+            AudioDataModel ii = new AudioDataModel() { Title = "RapGod", Duration = "00:00:00" };
+
+            int expected = i.GetHashCode();
+            int actual = ii.GetHashCode();
+
+            Assert.Equal(expected, actual);
         }
     }
 
@@ -153,6 +189,28 @@ namespace MusicPlayerTests
             string expected = "Title: RapGod | Duration: 00:00:00 | AudioFilePath: C://";
 
             string actual = testy.ToString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetHashCode_FromFilled_SameAsAnEqual()
+        {
+            AudioMetaData i = new AudioMetaData()
+            {
+                Title = "RapGod",
+                Duration = new TimeSpan(0, 0, 0),
+                AudioFilePath = "C://"
+            };
+            AudioMetaData ii = new AudioMetaData()
+            {
+                Title = "RapGod",
+                Duration = new TimeSpan(0, 0, 0),
+                AudioFilePath = "C://"
+            };
+
+            int expected = i.GetHashCode();
+            int actual = ii.GetHashCode();
 
             Assert.Equal(expected, actual);
         }
@@ -215,6 +273,30 @@ namespace MusicPlayerTests
             string expected = "AppStyle: DARK | AudioDevice: Klingel | MediaPath: C:// | AudioDevices: Klingel, Woover, ";
 
             string actual = appSettings.ToString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetHashCode_FromFilled_SameAsAnEqual()
+        {
+            AppSettings i = new AppSettings()
+            {
+                AppStyle = APPLICATION_STYLE.DARK,
+                AudioDevice = "Klingel",
+                AudioDevices = new List<string>() { "Klingel", "Woover" },
+                MediaPath = "C://"
+            };
+            AppSettings ii = new AppSettings()
+            {
+                AppStyle = APPLICATION_STYLE.DARK,
+                AudioDevice = "Klingel",
+                AudioDevices = new List<string>() { "Klingel", "Woover" },
+                MediaPath = "C://"
+            };
+
+            int expected = i.GetHashCode();
+            int actual = ii.GetHashCode();
 
             Assert.Equal(expected, actual);
         }
